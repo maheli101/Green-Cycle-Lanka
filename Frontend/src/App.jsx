fe7c7fbd8c8d8b0ecc2d531c907010afeb72aaaf
AnuradhiSiriwardhana <anuradhisiriwardhana123@gmail.com>
2024-05-13 15:32:09 +0530

    supplier function

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import maheli from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <h1>laka sampath</h1>
+      <img src="" alt="" />
     
     </>
   )
