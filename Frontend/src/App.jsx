4a86af8f181983057eb2b437bba424df773e7485
maheli101 <maheligeorge@gmail.com>
2024-05-17 12:08:25 +0530

    done with home page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import "./App.css";
+import BuyerSelectionPage from "./pages/BuyerSelectionPage";
+import HomePage from "./pages/HomePage";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>laka sampath</h1>
-    
-    </>
-  )
+    <div className="App">
+   <HomePage/>
+
+    </div>
+  );
 }
 
-export default App
+export default App;
