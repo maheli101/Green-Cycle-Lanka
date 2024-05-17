df6df443bec7dc7ae769760bf7a1404f174eaa86
maheli101 <maheligeorge@gmail.com>
2024-05-17 12:34:36 +0530

    update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,17 @@
-
-import "./App.css";
-import BuyerSelectionPage from "./pages/BuyerSelectionPage";
-import HomePage from "./pages/HomePage";
-
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-   <HomePage/>
+  const [count, setCount] = useState(0)
 
-    </div>
-  );
+  return (
+    <>
+      <h1>laka sampath</h1>
+    
+    </>
+  )
 }
 
-export default App;
+export default App
